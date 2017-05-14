3e22db24aa238e0f92efb4880f1bc0af04bff47b
Wasdns <952693358@qq.com>
2017-05-14 20:32:06 +0800

    add src
    add src

---
+++ src/show_sw_tables.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+from time import sleep
+import os
+import subprocess
+
+_THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+_THRIFT_BASE_PORT = 22222
+
+parser = argparse.ArgumentParser(description='P4 demo')
+parser.add_argument('--swname', help='Switch Name',
+                    type=str, action="store", required=True)
+args = parser.parse_args()
+
+def main():
+    # Get Thrift Port
+    sw_name = args.swname
+    index = int(sw_name[1:])-1
+    thrift_port = _THRIFT_BASE_PORT+index
+    
+    cmd = "./simple_switch_CLI --thrift-port %d < cmd/show_tables.txt" % thrift_port
+    os.system(cmd)
+
+if __name__ == '__main__':
+    main()
---
+++ src/show_table_info.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse
+from time import sleep
+import os
+import subprocess
+
+_THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+_THRIFT_BASE_PORT = 22222
+
+parser = argparse.ArgumentParser(description='P4 demo')
+parser.add_argument('--swname', help='Switch Name',
+                    type=str, action="store", required=True)
+parser.add_argument('--table-name', help='Table Name',
+                    type=str, action="store", required=True)
+args = parser.parse_args()
+
+def main():
+    # Get Thrift Port
+    sw_name = args.swname
+    index = int(sw_name[1:])-1
+    thrift_port = _THRIFT_BASE_PORT+index
+
+    # Get Table Name
+    table_name = args.table_name
+    
+    table_info_cmd = "echo 'table_info %s' > cmd/table_info.txt" % table_name
+    os.system(table_info_cmd)
+    cmd = "./simple_switch_CLI --thrift-port %d < cmd/table_info.txt" % thrift_port
+    os.system(cmd)
+    os.system("rm -rf cmd/table_info.txt")
+
+if __name__ == '__main__':
+    main()
